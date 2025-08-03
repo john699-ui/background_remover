bc67c35610109e40938653a44f9ce62823880e08
john699-ui <johndanielt@gmail.com>
2025-08-03 16:13:53 +0530

    Update mask_utils.js

--- js/mask_utils.js
+++ js/mask_utils.js
@@ -1,4 +1,4 @@
-po/*export function outputToMaskImage(output, width, height) {
+/*export function outputToMaskImage(output, width, height) {
   const imageData = new ImageData(width, height);
   for (let i = 0; i < width * height; i++) {
     const value = output[i] * 255;
